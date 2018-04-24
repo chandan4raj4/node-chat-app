3eb1091f130aeb55099743d71adaed4b29c3bd4e
Chandan <ckumar61@in.ibm.com>
2018-04-24 23:30:14 +0530

    add connect and disconnect event handelers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
+//https://blooming-sierra-62290.herokuapp.com/
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socket = require('socket.io');
+
 const currentPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+var server = http.createServer(app);
+var io = socket(server);
 
 app.use(express.static(currentPath));
 
+io.on('connection', (socket)=>{
+    console.log('New User Connected !!!');
+    socket.on('disconnect', ()=>{
+        console.log('User Disconnected from Server');
+    });
+});
 
-app.listen(port, ()=>{
+
+server.listen(port, ()=>{
     console.log(`Application Running on port : ${port}`);
 });
